4322fb953ebb935c9a872c86a6eab71f084fb790
Shaik Hussain <SARSHAD3@depaul.edu>
2021-09-28 00:33:35 -0500

    Add Task2

--- Assignment2/Assignment2.java
+++ Assignment2/Assignment2.java
@@ -2,6 +2,15 @@ class Assignment2{
     public static void main(String args[])
     {
        System.out.println("Print Task 1");
-      
+       player obj = new player();
+       obj.numPlayers();
     }
+}
+class player
+{
+	public int numPlayers()
+	{
+		System.out.println("Print Task 2");
+		return 0;
+	}
 }
