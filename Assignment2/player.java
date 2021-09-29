f1f4851f0feef087ccbf32e7ab9aa0751e69b1ab
Shaik Hussain <SARSHAD3@depaul.edu>
2021-09-28 19:44:30 -0500

    Task 6
    Retrive player.java

---
+++ Assignment2/player.java
@@ -0,0 +1,12 @@
+public class player
+{
+	public int numPlayers()
+	{
+		System.out.println("Print Task 2");
+	    PlayerData obj4 = new PlayerData();
+	    PlayerData[] myData;
+	    myData = obj4.getMyData();
+	    System.out.println("Print Task 4");
+		return 0;
+	}
+}
